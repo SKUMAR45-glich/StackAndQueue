0d6ea2d2b9e9bb9ed9bc168aad897f6133b03f41
Saurabh <saurabh.nit20@gmail.com>
2020-10-23 18:13:10 +0530

    [Saurabh]PusandDisplay

--- Program.cs
+++ Program.cs
@@ -7,7 +7,11 @@ namespace StackandQueue
         public static void Main(string[] args)
         {
             Console.WriteLine("Stack Implementation");
-            
+            LinkedListStack linkedListStack = new LinkedListStack();
+            linkedListStack.Push(70);
+            linkedListStack.Push(30);
+            linkedListStack.Push(56);
+            linkedListStack.Display();
         }
     }
 }
